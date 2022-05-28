162f113d931c06568a31d8a322c1dabed4677217
Meghajagadale7973 <jagadalemegha7973@gmail.com>
2022-05-28 17:58:18 +0530

    first commit

--- ConsoleApp1/ConsoleApp1/Class5.cs
+++ ConsoleApp1/ConsoleApp1/Class5.cs
@@ -4,9 +4,12 @@ using System.Text;
 
 namespace ConsoleApp1
 {
-    class Class4
+    class Class5
     {
-        static void Main2(String[] args)
+
+
+
+        static void Main5(String[] args)
         {
             Employee[] employeelist = new Employee[3];
             for (int i = 0; i < employeelist.Length; i++)
@@ -21,20 +24,31 @@ namespace ConsoleApp1
                 employeelist[i].PrintData();
             }
 
-    }
+        }
         class Employee
         {
             public string ename;
-            public int Eid;
+            public int eid;
             public String Egender;
             public int esalary;
+            public string companyname;
+           public Employee()
+            {
+                companyname = "capgemini";
+            }
+
+            ~ Employee()
+            {
+
+            }
+
             public void getData()
             {
                 Console.WriteLine("Please enter employee details");
                 Console.WriteLine("please enter employee name");
                 ename = Console.ReadLine();
-                Console.WriteLine("Please enter Eid");
-                Eid = Convert.ToInt32(Console.ReadLine());
+               Console.WriteLine("Please enter Eid");
+               eid = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Please enter Egender");
                 Egender = Console.ReadLine();
                 Console.WriteLine("Please enter Esalary");
@@ -46,16 +60,12 @@ namespace ConsoleApp1
             {
                 Console.WriteLine("Employee Details are");
                 Console.WriteLine(ename);
-                Console.WriteLine(Eid);
+                Console.WriteLine(eid);
                 Console.WriteLine(Egender);
                 Console.WriteLine(esalary);
+                Console.WriteLine(companyname);
 
             }
         }
-
-        
-       
-
-    
     }
 }
