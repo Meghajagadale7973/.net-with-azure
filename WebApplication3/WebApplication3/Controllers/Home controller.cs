0173e2701fe72aaab8ea58ee9a3e1137246d112e
Meghajagadale7973 <jagadalemegha7973@gmail.com>
2022-06-08 23:04:40 +0530

    initial commit

---
+++ WebApplication3/WebApplication3/Controllers/Home controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Homecontroller : ControllerBase
+    {
+
+        public List<Etable> Get()
+        {
+            sampledb2Context db = new sampledb2Context();
+            return db.Etables.ToList();
+
+        }
+    }
+}
